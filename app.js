1b617f92076a9fff5792fd9ca49fdf1c1f4791a1
AntonioTamez <antonio.tamez@hotmail.com>
2025-02-04 19:31:40 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+import routesMascotas from './routes/mascotas.js';
+
+const app = express();
+app.use('/mascotas', routesMascotas);
+
+try {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, ()=> console.log('Servidor activo en el puerto ' + PORT));
+} catch (error) {
+    console.log(error);
+}
---
+++ routes/mascotas.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import mascotasController from '../controllers/mascotas.js'
+
+const route = express.Router();
+
+route.post('/', mascotasController.create);
+route.get('/:id', mascotasController.getOne);
+route.get('/', mascotasController.getAll);
+route.put('/', mascotasController.update);
+route.delete('/:id', mascotasController.delete);
+
+export default route;
